07a3126e3ffd084ce5d02c68709a144d56dda7ed
markzatopek <157063706+markzatopek@users.noreply.github.com>
2024-02-08 18:12:33 -0600

    Fixed Small Format Errors

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,7 +1,7 @@
 def telephone():
     phone_number = int(input())
     area_code = "(" + str(int(phone_number / 10000000)) + ") "
-    body = str(int((phone_number % 10000000) / 10000)) + " - " + str(phone_number % 10000)
+    body = str(int((phone_number % 10000000) / 10000)) + "-" + str(phone_number % 10000)
     print(area_code + body)
     
 if __name__ == "__main__":
--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -5,9 +5,9 @@ def half_life(mg, hour):
 
 def caffeine():
     caffeine_mg = float(input())
-    print(f"After 6 hours: {half_life(caffeine_mg, 6):.2f}")
-    print(f"After 12 hours: {half_life(caffeine_mg, 12):.2f}")
-    print(f"After 24 hours: {half_life(caffeine_mg, 24):.2f}")
+    print(f"After 6 hours: {half_life(caffeine_mg, 6):.2f} mg")
+    print(f"After 12 hours: {half_life(caffeine_mg, 12):.2f} mg")
+    print(f"After 24 hours: {half_life(caffeine_mg, 24):.2f} mg")
     
     
 if __name__ == "__main__":
